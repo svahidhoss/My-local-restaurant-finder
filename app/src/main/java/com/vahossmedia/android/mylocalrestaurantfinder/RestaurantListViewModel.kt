2e31aee9d94820ef6b009b0ccb02f1cc5c7f0776
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-12-04 22:47:38 -0800

    Adds RestaurantDetailFragment.kt and its layout. Also adds RestaurantListViewModel and exposes a mock list of restaurants as a state flow. Adds dependencies for coroutines.

---
+++ app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/RestaurantListViewModel.kt
@@ -0,0 +1,38 @@
+package com.vahossmedia.android.mylocalrestaurantfinder
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.util.UUID
+
+class RestaurantListViewModel : ViewModel() {
+
+    private val _restaurants = MutableStateFlow<List<Restaurant>>(emptyList())
+
+    val restaurants
+        get() = _restaurants.asStateFlow()
+
+    init {
+        val mockCrimes = mutableListOf<Restaurant>()
+        for (i in 1 until 50) {
+            val restaurant = Restaurant(
+                UUID.randomUUID(),
+                "Yaas",
+                "A Persian grill house",
+                4.0,
+                ""
+            )
+            mockCrimes.add(restaurant)
+        }
+
+        viewModelScope.launch {
+            updateRestaurants(mockCrimes)
+        }
+    }
+
+    private suspend fun updateRestaurants(newItems: List<Restaurant>) {
+        _restaurants.emit(newItems)
+    }
+}
