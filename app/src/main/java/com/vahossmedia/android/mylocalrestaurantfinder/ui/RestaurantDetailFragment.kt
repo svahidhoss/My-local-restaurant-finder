13b815541217bf2fe4046c7fe3f6e0c1fb4286fb
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-12-05 15:56:00 -0800

    Uses a mock Restaurant to update the UI on the onViewCreated method.

--- app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/ui/RestaurantDetailFragment.kt
+++ app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/ui/RestaurantDetailFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.vahossmedia.android.mylocalrestaurantfinder.Restaurant
 import com.vahossmedia.android.mylocalrestaurantfinder.databinding.FragmentRestaurantDetailBinding
+import java.util.UUID
 
 /**
  * Represents Detail view of selected restaurant.
@@ -26,6 +28,26 @@ class RestaurantDetailFragment : Fragment() {
         // Inflate the layout for this fragment using binding
         _binding = FragmentRestaurantDetailBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val restaurant = Restaurant(
+            UUID.randomUUID(),
+            "Yaas",
+            "A Persian grill house",
+            4.0,
+            ""
+        )
+
+        updateUi(restaurant)
+    }
+
+    private fun updateUi(restaurant: Restaurant) {
+        binding.apply {
+            binding.restaurantName.text = restaurant.name
+            binding.restaurantRating.rating = restaurant.rating.toFloat()
+        }
     }
 
     override fun onDestroy() {
