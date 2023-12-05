e3cc2a4c6d643ea9b8f21815381c53e6c2097663
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-12-04 23:03:52 -0800

    Adds recyclerview dep. Makes fragment_restaurant_list.xml a recycler view. Uses view binding for RestaurantListFragment.

--- app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/RestaurantListFragment.kt
+++ app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/RestaurantListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.vahossmedia.android.mylocalrestaurantfinder.databinding.FragmentRestaurantListBinding
 
 /**
  * A simple [Fragment] subclass that displays
@@ -12,11 +13,25 @@ import android.view.ViewGroup
  */
 class RestaurantListFragment : Fragment() {
 
+    private var _binding: FragmentRestaurantListBinding? = null
+
+    // Make binding a computed value
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_list, container, false)
+    ): View {
+        // Inflate the layout for this fragment using binding
+        _binding = FragmentRestaurantListBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
