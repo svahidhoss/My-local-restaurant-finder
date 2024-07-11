0043d0cd4b9562adeb0663c64f9d94f80ed7bc25
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2024-07-11 00:52:29 -0700

    Adds MyLocalRestaurantFinderApplication class. Puts back gradle.properties file.

---
+++ app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/MyLocalRestaurantFinderApplication.kt
@@ -0,0 +1,9 @@
+package com.vahossmedia.android.mylocalrestaurantfinder
+
+import android.app.Application
+
+class MyLocalRestaurantFinderApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
