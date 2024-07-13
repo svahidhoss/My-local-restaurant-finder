2da480695e8a8b3c79562715c626667fa5a85b88
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2024-07-12 17:49:27 -0700

    Updates the data classes. M

--- app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/model/Business.kt
+++ app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/model/Business.kt
@@ -3,20 +3,21 @@ package com.vahossmedia.android.mylocalrestaurantfinder.model
 import com.squareup.moshi.Json
 
 data class Business(
-    @Json(name = "id") val id: String,
-    @Json(name = "alias") val alias: String,
-    @Json(name = "name") val name: String,
-    @Json(name = "image_url") val imageUrl: String,
+    val id: String,
+    val alias: String?,
+    val name: String,
+    val imageUrl: String?,
     @Json(name = "is_closed") val isClosed: Boolean,
-    @Json(name = "url") val url: String,
-    @Json(name = "review_count") val reviewCount: Int,
-    @Json(name = "categories") val categories: List<Category>,
-    @Json(name = "rating") val rating: Double,
-    val coordinates: Coordinates,
-    val transactions: List<String>,
-    val price: String,
-    val location: Location,
-    val phone: String,
-    @Json(name = "display_phone") val displayPhone: String,
-    val distance: Double
+    val description: String? = null,
+    val rating: Double = 0.0,
+    val url: String?,
+    val reviewCount: Int?,
+    val categories: List<Category>?,
+    val coordinates: Coordinates?,
+    val transactions: List<String>?,
+    val price: String?,
+    val location: Location?,
+    val phone: String?,
+    @Json(name = "display_phone") val displayPhone: String?,
+    val distance: Double?
 )
--- app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/model/Location.kt
+++ app/src/main/java/com/vahossmedia/android/mylocalrestaurantfinder/model/Location.kt
@@ -4,11 +4,11 @@ import com.squareup.moshi.Json
 
 data class Location(
     val address1: String,
-    val address2: String?,
-    val address3: String?,
+    val address2: String = "",
+    val address3: String = "",
     val city: String,
     @Json(name = "zip_code") val zipCode: String,
     val country: String,
     val state: String,
-    @Json(name = "display_address") val displayAddress: List<String>
+    @Json(name = "display_address") val displayAddress: List<String> = emptyList()
 )
